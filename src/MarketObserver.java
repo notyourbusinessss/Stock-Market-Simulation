2b7fa099d09fa8a58254a7df909cc1f6aec66462
max <maxencelamontagne03+github@gmail.com>
2025-04-24 19:54:49 -0400

    Javadoc CustomWindowPanel

--- src/MarketObserver.java
+++ src/MarketObserver.java
@@ -1,3 +1,14 @@
+/**
+ * Interface for observers that want to be notified about changes in the market state.
+ * Implementing classes will receive updates when the number of shares or the stock price changes.
+ */
 public interface MarketObserver {
+
+    /**
+     * Called when the market updates its state.
+     *
+     * @param shares the number of shares currently available in the market
+     * @param price  the current price of the stock
+     */
     void updateMarketState(int shares, double price);
 }
